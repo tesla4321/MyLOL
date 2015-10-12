6c8d79983870a825dcf4c091cd9772fbeed67cbb
tesla4321 <tsing_x@icloud.com>
2015-10-12 14:57:20 +0800

    Initial Commit

---
+++ MyLOL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLOL
+//
+//  Created by kayle on 15/10/12.
+//  Copyright © 2015年 bestkayle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyLOL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyLOL
+//
+//  Created by kayle on 15/10/12.
+//  Copyright © 2015年 bestkayle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
